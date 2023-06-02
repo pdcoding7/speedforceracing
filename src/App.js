36e4a3c33a16309f94e12a6484f48e445cfaf9df
Paul Doran <paul.doran2@theverygroup.com>
2023-06-02 13:42:21 +0100

    alternate white/black backgrounds. amended copy to black where applicable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import Navbar from './components/Navbar';
-import Standings from './components/Standings';
-import PastStreams from './components/PastStreams';
-import Gallery from './components/Gallery';
-import HallOfFame from './components/HallOfFame';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
-
+import Navbar from "./components/Navbar";
+import Standings from "./components/Standings";
+import PastStreams from "./components/PastStreams";
+import Gallery from "./components/Gallery";
+import HallOfFame from "./components/HallOfFame";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
-  return <> 
-    <Navbar>    
-    </Navbar>
-    <Hero />
-    <Standings />
-    <PastStreams />
-    <Gallery />
-    <HallOfFame />
-    <Footer />  
-  </>;
-
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero />
+      <Standings />
+      <PastStreams />
+      <Gallery />
+      <HallOfFame />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
-
-
-
