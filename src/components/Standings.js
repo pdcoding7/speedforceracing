e9bba3d37658b1bf259e9b2e6c148340ec4ce564
Paul Doran <paul.doran2@theverygroup.com>
2023-09-29 16:31:45 +0100

    Updated past streams, standings and added constructors tables

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -7,14 +7,17 @@ import { constructorsGraphicGp1 } from "../data";
 import { standingsGP2Config } from "../data";
 import { standingsRealisticConfig } from "../data";
 import DivisionTitle from "./DivisionTitle.js";
-// import { constructorsAllGraphic } from "../data";
+import { constructorsAllGraphic } from "../data";
+import { constructorsConfig } from "../data";
+import { constructorsGP2Config } from "../data";
+import { constructorsAllConfig } from "../data";
 
 const Standings = () => {
 	return (
 		<section className="section" id="standings">
 			<div id="gp1Standings">
 				<SectionTitle sectionTitle="Standings"></SectionTitle>
-				<DivisionTitle division="Div 1" typeOf="- Race Result" />
+				{/* <DivisionTitle division="Div 1" typeOf="- Race Result" />
 				<div className="fameFlex">
 					{raceResultsDiv1.map((seasonNum) => {
 						const { id, season, src } = seasonNum;
@@ -26,7 +29,7 @@ const Standings = () => {
 							</div>
 						);
 					})}
-				</div>
+				</div> */}
 
 				<DivisionTitle division="Div 1" typeOf="- Driver Standings" />
 				<table>
@@ -78,7 +81,7 @@ const Standings = () => {
 				</table>
 
 				<DivisionTitle division="Div 1" typeOf="- Constructors Championship" />
-				<div className="fameFlex">
+				{/* <div className="fameFlex">
 					{constructorsGraphicGp1.map((seasonNum) => {
 						const { id, season, src } = seasonNum;
 						return (
@@ -89,69 +92,50 @@ const Standings = () => {
 							</div>
 						);
 					})}
-				</div>
+				</div> */}
 
-				{/* <table className="constructors">
-          <tr>
-            <th className="positionCol">
-              <h4>Pos</h4>
-            </th>
-            <th className="teamCol">
-              <h4>Team</h4>
-            </th>
-            <th className="columnHide">
-              <h4>1st</h4>
-            </th>
-            <th className="columnHide">
-              <h4>2nd</h4>
-            </th>
-            <th className="columnHide">
-              <h4>3rd</h4>
-            </th>
-            <th className="columnHide">
-              <h4>FL</h4>
-            </th>
-            <th>
-              <h4>Points</h4>
-            </th>
-          </tr>
-          {constructorsConfig.map((constructors) => {
-            const {
-              id,
-              position,
-              team,
-              teamBadge,
-              wins,
-              second,
-              third,
-              fastest,
-              points,
-            } = constructors;
-            return (
-              <tr key={id}>
-                <td>
-                  <p>{position}</p>
-                </td>
-                <td>
-                  <div className="flexBox">
-                    <h5>{team}</h5>
-                    <div class="teamImg">
-                      <img src={teamBadge} alt="" />
-                    </div>
-                  </div>
-                </td>
-                <td className="columnHide">{wins}</td>
-                <td className="columnHide">{second}</td>
-                <td className="columnHide">{third}</td>
-                <td className="fastestCol columnHide">{fastest}</td>
-                <td>{points}</td>
-              </tr>
-            );
-          })}
-        </table> */}
+				<table className="constructors">
+					<tr>
+						<th className="positionCol">
+							<h4>Pos</h4>
+						</th>
+						<th className="teamCol">
+							<h4>Team</h4>
+						</th>
+						<th className="blankCol"></th>
+						<th className="columnHide">
+							<h4>Wins</h4>
+						</th>
+						<th>
+							<h4>Points</h4>
+						</th>
+					</tr>
+					{constructorsConfig.map((constructors) => {
+						const { id, position, teamBadge, team, wins, points } = constructors;
+						return (
+							<tr key={id}>
+								<td>
+									<p>{position}</p>
+								</td>
+								<td>
+									<div className="flexBox">
+										<div class="teamImg">
+											<img src={teamBadge} alt="" />
+										</div>
+									</div>
+								</td>
+								<td className="blankCol">
+									<h3>{team}</h3>
+								</td>
+								<td>{wins}</td>
+								<td>{points}</td>
+							</tr>
+						);
+					})}
+				</table>
 			</div>
 			<div id="gp2Standings">
-				<DivisionTitle division="Div 2" typeOf="- Race Result" />
+				{/* <DivisionTitle division="Div 2" typeOf="- Race Result" />
 				<div className="fameFlex">
 					{raceResultsDiv2.map((seasonNum) => {
 						const { id, season, src } = seasonNum;
@@ -163,7 +147,7 @@ const Standings = () => {
 							</div>
 						);
 					})}
-				</div>
+				</div> */}
 				<DivisionTitle division="Div 2" typeOf="- Driver Standings" />
 				<table>
 					<tr>
@@ -213,8 +197,8 @@ const Standings = () => {
 					})}
 				</table>
 
-				<DivisionTitle division="Div 2" typeOf="Constructors Championship" />
-				<div className="fameFlex">
+				<DivisionTitle division="Cross Div" typeOf="Constructors Championship" />
+				{/* <div className="fameFlex">
 					{constructorsGraphic.map((seasonNum) => {
 						const { id, season, src } = seasonNum;
 						return (
@@ -225,66 +209,47 @@ const Standings = () => {
 							</div>
 						);
 					})}
-				</div>
+				</div> */}
 
-				{/* <table className="constructors">
-          <tr>
-            <th className="positionCol">
-              <h4>Pos</h4>
-            </th>
-            <th className="teamCol">
-              <h4>Team</h4>
-            </th>
-            <th className="columnHide">
-              <h4>1st</h4>
-            </th>
-            <th className="columnHide">
-              <h4>2nd</h4>
-            </th>
-            <th className="columnHide">
-              <h4>3rd</h4>
-            </th>
-            <th className="columnHide">
-              <h4>FL</h4>
-            </th>
-            <th>
-              <h4>Points</h4>
-            </th>
-          </tr>
-          {constructorsGP2Config.map((constructors) => {
-            const {
-              id,
-              position,
-              team,
-              teamBadge,
-              wins,
-              second,
-              third,
-              fastest,
-              points,
-            } = constructors;
-            return (
-              <tr key={id}>
-                <td>
-                  <p>{position}</p>
-                </td>
-                <td>
-                  <div className="flexBox">
-                    <h5>{team}</h5>
-                    <div class="teamImg">
-                      <img src={teamBadge} alt="" />
-                    </div>
-                  </div>
-                </td>
-                <td className="columnHide">{wins}</td>
-                <td className="columnHide">{second}</td>
-                <td className="columnHide">{third}</td>
-                <td className="fastestCol columnHide">{fastest}</td>
-                <td>{points}</td>
-              </tr>
-            );
-          })}
-        </table> */}
+				<table className="constructors">
+					<tr>
+						<th className="positionCol">
+							<h4>Pos</h4>
+						</th>
+						<th className="teamCol">
+							<h4>Team</h4>
+						</th>
+						<th className="blankCol"></th>
+						<th className="columnHide">
+							<h4>Wins</h4>
+						</th>
+						<th>
+							<h4>Points</h4>
+						</th>
+					</tr>
+					{constructorsGP2Config.map((constructors) => {
+						const { id, position, teamBadge, team, wins, points } = constructors;
+						return (
+							<tr key={id}>
+								<td>
+									<p>{position}</p>
+								</td>
+								<td>
+									<div className="flexBox">
+										<div class="teamImg">
+											<img src={teamBadge} alt="" />
+										</div>
+									</div>
+								</td>
+								<td className="blankCol">
+									<h3>{team}</h3>
+								</td>
+								<td>{wins}</td>
+								<td>{points}</td>
+							</tr>
+						);
+					})}
+				</table>
 			</div>
 
 			<div id="rpStandings">
@@ -332,10 +297,10 @@ const Standings = () => {
 				</table>
 			</div>
 
-			{/* <div id="crossDivStandings">
-				<DivisionTitle division="Cross Div" typeOf="Constructors" />
-				<div className="fameFlex">
-					{constructorsAllGraphic.map((seasonNum) => {
+			<div id="crossDivStandings">
+				<DivisionTitle division="Cross Div" typeOf="Constructors Championship" />
+				{/* <div className="fameFlex">
+					{constructorsGraphic.map((seasonNum) => {
 						const { id, season, src } = seasonNum;
 						return (
 							<div className="flexDiv">
@@ -345,8 +310,52 @@ const Standings = () => {
 							</div>
 						);
 					})}
-				</div>
-			</div> */}
+				</div> */}
+
+				<table className="constructors">
+					<tr>
+						<th className="positionCol">
+							<h4>Pos</h4>
+						</th>
+						<th className="teamCol">
+							<h4>Team</h4>
+						</th>
+						<th className="blankCol"></th>
+						<th className="columnHide">
+							<h4>Div 1</h4>
+						</th>
+						<th>
+							<h4>Div 2</h4>
+						</th>
+						<th>
+							<h4>Total</h4>
+						</th>
+					</tr>
+					{constructorsAllConfig.map((constructors) => {
+						const { id, position, teamBadge, team, div1, div2, totalPoints } = constructors;
+						return (
+							<tr key={id}>
+								<td>
+									<p>{position}</p>
+								</td>
+								<td>
+									<div className="flexBox">
+										<div class="teamImg">
+											<img src={teamBadge} alt="" />
+										</div>
+									</div>
+								</td>
+								<td className="blankCol">
+									<h3>{team}</h3>
+								</td>
+								<td>{div1}</td>
+								<td>{div2}</td>
+								<td>{totalPoints}</td>
+							</tr>
+						);
+					})}
+				</table>
+			</div>
 		</section>
 	);
 };
