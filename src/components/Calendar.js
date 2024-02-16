737d2b252c230c1db31e4f9b374f5effd9ba15ac
Paul Doran <paul.doran2@theverygroup.com>
2024-02-16 15:50:13 +0000

    updated standings and past streams. added tab functionality to calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,25 @@
 import CalendarComponent from "./CalendarComponent";
 import SectionTitle from "./SectionTitle";
+import Tabs from "./Tabs";
 
 const Calendar = () => {
 	return (
 		<section className="section" id="calendar">
 			<SectionTitle sectionTitle="Race Calendar"></SectionTitle>
-			<CalendarComponent src="/images/s14-calender-div-1-s15.png" />
-			<CalendarComponent src="/images/s14-calender-div-2-s15.png" />
+			<Tabs
+				tabs={[
+					{
+						id: 1,
+						tabTitle: "Division 1",
+						content: <CalendarComponent src="/images/s14-calender-div-1-s15.png" />,
+					},
+					{
+						id: 2,
+						tabTitle: "Division 2",
+						content: <CalendarComponent src="/images/s14-calender-div-2-s15.png" />,
+					},
+				]}
+			/>
 		</section>
 	);
 };
--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,11 +1,55 @@
 import SectionTitle from "./SectionTitle.js";
 import Tabs from "./Tabs.js";
+import StandingsComponent from "./StandingsComponent";
+import StandingsContructorsComponent from "./StandingsContructorsComponent.js";
+import DivisionTitle from "./DivisionTitle.js";
+import StandingsOverallComponent from "./StandingsOverallComponent.js";
 
 const Standings = () => {
 	return (
 		<section className="section" id="standings">
 			<SectionTitle sectionTitle="Standings"></SectionTitle>
-			<Tabs />
+			<Tabs
+				tabs={[
+					{
+						id: 1,
+						tabTitle: "Division 1",
+						content: (
+							<>
+								<DivisionTitle division="Division 1" typeOf="- Driver Standings" id="#gp1Standings" />
+								<StandingsComponent league={1} />
+								<DivisionTitle division="Division 1" typeOf="- Constructors Standings" />
+								<StandingsContructorsComponent league={1} />
+							</>
+						),
+						defaultTab: true,
+					},
+					{
+						id: 2,
+						tabTitle: "Division 2",
+						content: (
+							<>
+								<DivisionTitle division="Division 2" typeOf="- Driver Standings" id="#gp1Standings" />
+								<StandingsComponent league={2} />
+								<DivisionTitle division="Division 2" typeOf="- Constructors Standings" />
+								<StandingsContructorsComponent league={2} />
+							</>
+						),
+						defaultTab: false,
+					},
+					{
+						id: 3,
+						tabTitle: "Overall",
+						content: (
+							<>
+								<DivisionTitle division="Cross Div" typeOf="- Constructors Standings" />
+								<StandingsOverallComponent />
+							</>
+						),
+						defaultTab: false,
+					},
+				]}
+			/>
 		</section>
 	);
 };
